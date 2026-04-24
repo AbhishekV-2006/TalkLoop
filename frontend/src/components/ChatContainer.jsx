d3378d5cb4b61fcc27998f1000aa0355f7e1ed43
AbhishekV-2006 <codeabhishek4396@gmail.com>
2026-04-24 17:52:45 +0530

    sendmessage function

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -7,21 +7,23 @@ import MessageInput from "./MessageInput";
 import MessagesLoadingSkeleton from "./MessagesLoadingSkeleton";
 
 function ChatContainer() {
-
   const { selectedUser, getMessagesByUserId, messages, isMessagesLoading } = useChatStore();
-
   const { authUser } = useAuthStore();
   const messageEndRef = useRef(null);
 
   useEffect(() => {
-    if (selectedUser) {
-      getMessagesByUserId(selectedUser._id);
+    getMessagesByUserId(selectedUser._id);
+  }, [selectedUser, getMessagesByUserId]);
+
+  useEffect(() => {
+    if (messageEndRef.current) {
+      messageEndRef.current.scrollIntoView({ behavior: "smooth" });
     }
-  }, [selectedUser,getMessagesByUserId]);
+  }, [messages]);
 
   return (
     <>
-      <ChatHeader/>
+      <ChatHeader />
       <div className="flex-1 px-6 overflow-y-auto py-8">
         {messages.length > 0 && !isMessagesLoading ? (
           <div className="max-w-3xl mx-auto space-y-6">
@@ -42,11 +44,16 @@ function ChatContainer() {
                   )}
                   {msg.text && <p className="mt-2">{msg.text}</p>}
                   <p className="text-xs mt-1 opacity-75 flex items-center gap-1">
-                    {new Date(msg.createdAt).toISOString().slice(11, 16)}
+                    {new Date(msg.createdAt).toLocaleTimeString(undefined, {
+                      hour: "2-digit",
+                      minute: "2-digit",
+                    })}
                   </p>
                 </div>
               </div>
             ))}
+            {/* 👇 scroll target */}
+            <div ref={messageEndRef} />
           </div>
         ) : isMessagesLoading ? (
           <MessagesLoadingSkeleton />
@@ -57,7 +64,7 @@ function ChatContainer() {
 
       <MessageInput />
     </>
-  )
+  );
 }
 
-export default ChatContainer
+export default ChatContainer;
