58d1fb5b16b7210a0812c97b31bcde3a655e6e5b
AbhishekV-2006 <codeabhishek4396@gmail.com>
2026-07-03 17:53:56 +0530

    changes

--- backend/src/emails/emailHandlers.js
+++ backend/src/emails/emailHandlers.js
@@ -1,19 +1,22 @@
-// import { resendClient, sender } from "../lib/resend.js";
 import { createWelcomeEmailTemplate } from "../emails/emailTemplates.js";
 import nodemailer from "nodemailer";
+import { ENV } from "../lib/env.js";
+
 export const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: process.env.NODEMAILER_EMAIL, // your Gmail address
-    pass: process.env.NODEMAILER_PASSWORD, // your Gmail password or app password
+    user: ENV.NODEMAILER_EMAIL,
+    pass: ENV.NODEMAILER_PASSWORD,
   },
 });
+
+const sender = ENV.NODEMAILER_EMAIL;
 
 
 export const sendWelcomeEmail = async (email, name, clientURL) => {
 
   const mailOptions = {
-    from: `"TalkLoop" <talkloop@talkloop.com>`,
+    from: `"TalkLoop" <${sender}>`,
     to: email,
     subject: "Welcome to TalkLoop!",
     text: `Hello ${name},\n\nWelcome to TalkLoop! We're excited to have you on board`,
@@ -21,18 +24,4 @@ export const sendWelcomeEmail = async (email, name, clientURL) => {
   };
 
   await transporter.sendMail(mailOptions)
-
-  // const { data, error } = await resendClient.emails.send({
-  //   from: `${sender.name} <${sender.email}>`,
-  //   to: email,
-  //   subject: "Welcome to TalkLoop!",
-  //   html: createWelcomeEmailTemplate(name, clientURL),
-  // });
-
-  // if (error) {
-  //   console.error("Error sending welcome email:", error);
-  //   throw new Error("Failed to send welcome email");
-  // }
-
-  // console.log("Welcome Email sent successfully", data);
 };
